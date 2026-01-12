9db5ecc0f6daf4d775c15e37a4c72a71b15540b8
Roman <dev@rman.dev>
2026-01-12 20:22:46 +0000

    fix build

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,15 @@
 import solid from "@astrojs/solid-js";
-import { serwist as pwa } from "@serwist/astro";
+import { serwist } from "@serwist/astro";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   integrations: [
     solid(),
-    pwa({ type: "module", globPatterns: ["**/*.{js,css,html,svg,png,ico}"] }),
+    serwist({
+      swSrc: "src/sw.ts",
+      globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+    }),
   ],
   vite: { plugins: [tailwindcss()] },
 });
