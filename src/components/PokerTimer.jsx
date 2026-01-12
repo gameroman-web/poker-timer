816a62977f31a6823aa76831a0da1ac056ea699b
Roman <dev@rman.dev>
2026-01-12 00:14:57 +0000

    Update PokerTimer.jsx

--- src/components/PokerTimer.jsx
+++ src/components/PokerTimer.jsx
@@ -86,28 +86,6 @@ const PokerTimer = () => {
 			setSmallBlind(levels[newLevel - 1].sb);
 			setBigBlind(levels[newLevel - 1].bb);
 			setTimeLeft(levels[newLevel - 1].time);
-
-			// Play notification sound (using Web Audio API for simple beep)
-			const audioContext = new (
-				window.AudioContext || window.webkitAudioContext
-			)();
-			const oscillator = audioContext.createOscillator();
-			const gainNode = audioContext.createGain();
-
-			oscillator.connect(gainNode);
-			gainNode.connect(audioContext.destination);
-
-			oscillator.frequency.value = 800;
-			oscillator.type = "sine";
-
-			gainNode.gain.setValueAtTime(0.3, audioContext.currentTime);
-			gainNode.gain.exponentialRampToValueAtTime(
-				0.01,
-				audioContext.currentTime + 0.5,
-			);
-
-			oscillator.start(audioContext.currentTime);
-			oscillator.stop(audioContext.currentTime + 0.5);
 		} else {
 			setIsRunning(false);
 			clearInterval(interval);
