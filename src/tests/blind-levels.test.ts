227f8df456272a09460298b9b9a3976b877e3344
Roman <dev@rman.dev>
2026-01-14 21:10:45 +0000

    update tests

--- src/tests/blind-levels.test.ts
+++ src/tests/blind-levels.test.ts
@@ -3,8 +3,41 @@ import { describe, expect, it } from "bun:test";
 import { getBlindLevels } from "../lib/blind-levels";
 
 describe("getBlindLevels", () => {
-  it("should return the correct blind levels", () => {
+  it("should correctly return for the 1st round", () => {
     expect(getBlindLevels({ first: 5, rounds: 1 })).toEqual([5]);
+    expect(getBlindLevels({ first: 10, rounds: 1 })).toEqual([10]);
+    expect(getBlindLevels({ first: 25, rounds: 1 })).toEqual([25]);
+  });
+
+  it("should correctly double for the 2nd round", () => {
     expect(getBlindLevels({ first: 5, rounds: 2 })).toEqual([5, 10]);
+    expect(getBlindLevels({ first: 10, rounds: 2 })).toEqual([10, 20]);
+    expect(getBlindLevels({ first: 25, rounds: 2 })).toEqual([25, 50]);
+  });
+
+  it("should correctly calculate for the 3rd round", () => {
+    expect(getBlindLevels({ first: 25, rounds: 3 })).toEqual([25, 50, 75]);
+  });
+
+  it("should correctly calculate for the 4th round", () => {
+    expect(getBlindLevels({ first: 25, rounds: 4 })).toEqual([25, 50, 75, 100]);
+  });
+
+  it("should correctly calculate for the 5th round", () => {
+    expect(getBlindLevels({ first: 25, rounds: 5 })).toEqual([
+      25, 50, 75, 100, 150,
+    ]);
+  });
+
+  it("should correctly calculate for the 5th round", () => {
+    expect(getBlindLevels({ first: 25, rounds: 6 })).toEqual([
+      25, 50, 75, 100, 150, 200,
+    ]);
+  });
+
+  it("should all be a multiple of the 1st round", () => {
+    expect(getBlindLevels({ first: 25, rounds: 20 })).toSatisfy((arr) =>
+      arr.every((v) => (v % 25) === 0),
+    );
   });
 });
