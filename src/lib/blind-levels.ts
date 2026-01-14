68a14cc3effa3977f566e6e97e54978b4af8d977
Roman <dev@rman.dev>
2026-01-14 21:36:56 +0000

    wip

--- src/lib/blind-levels.ts
+++ src/lib/blind-levels.ts
@@ -11,10 +11,28 @@ export function getBlindLevels({
   for (let i = 0; i < rounds; i++) {
     levels.push(currentSmall);
 
-    if (i >= 3) {
-      currentSmall = currentSmall + first * 2;
-    } else {
+    if (i < 3) {
       currentSmall = currentSmall + first;
+    } else if (i === 3) {
+      currentSmall = currentSmall + first * 2;
+    } else if (i === 4) {
+      currentSmall = currentSmall + first * 2;
+    } else if (i === 5) {
+      currentSmall = currentSmall + first * 4;
+    } else if (i === 6) {
+      currentSmall = currentSmall + first * 8;
+    } else if (i === 7) {
+      currentSmall = currentSmall + first * 12;
+    } else if (i === 8) {
+      currentSmall = currentSmall + first * 8;
+    } else if (i === 9) {
+      currentSmall = currentSmall + first * 20;
+    } else if (i === 10) {
+      currentSmall = currentSmall + first * 20;
+    } else if (i === 11) {
+      currentSmall = currentSmall + first * 40;
+    } else {
+      currentSmall = currentSmall + first * 40;
     }
   }
 
